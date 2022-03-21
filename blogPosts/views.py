a1079a07e77eaacf16a041f1ec7c0fae01f067d6
tasel220 <345t@naver.com>
2022-03-21 21:11:37 +0900

    0 django

--- blogPosts/views.py
+++ blogPosts/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'blogPosts/index.html')
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        return render(request, 'blogPosts/index.html', {'posts': posts})
+    elif request.method == 'POST':  
+        title = request.POST['title']
+        content = request.POST['content']
+        Post.objects.create(title=title, content=content)
+        return redirect('blogPosts:index')
+
+def new(request):
+    return render(request, 'blogPosts./new.html')
+
+def show(request, id):
+    post = Post.objects.get(id=id)
+    return render(request, 'blogPosts/show.html', {'post':post})
+
+def delete(request, id):
+    post = Post.objects.get(id=id)
+    post.delete() # 선택된 모델 인스턴스를 삭제하는 query 함수입니다.
+    return redirect('blogPosts:index')
--- seminar/urls.py
+++ seminar/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
 import blogPosts.views
+
+app_name = 'blogPosts'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogPosts.views.index, name='index'),
+    path('posts/', include('blogPosts.urls')),
 ]
